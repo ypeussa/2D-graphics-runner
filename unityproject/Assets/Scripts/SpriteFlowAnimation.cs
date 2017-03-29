9351d050634e02e57700d12742bbb6fd6eee158a
yrjo.peussa <peussa@iki.fi>
2017-03-29 19:47:44 +0300

    UV coordinate animations (waterfalls), fixed particle system sorting, tuned parallax values

---
+++ unityproject/Assets/Scripts/SpriteFlowAnimation.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// [ExecuteInEditMode]
+public class SpriteFlowAnimation : MonoBehaviour {
+    public float scrollSpeed = 0.5f;
+    public string sortingLayerName;
+    public int sortingOrder = 1;
+    public float textureScale = 1;
+
+    Renderer rend;
+
+	void Start () {
+        rend = GetComponent<Renderer>();
+	}
+	
+	void Update () {
+        rend.sortingLayerName = sortingLayerName;
+        rend.sortingOrder = sortingOrder;
+        float offset = Time.time * scrollSpeed;
+        rend.material.mainTextureOffset = new Vector2(0, offset);
+        var ls = transform.localScale;
+        rend.material.mainTextureScale = new Vector2(ls.x, ls.y) * textureScale;
+    }
+}
