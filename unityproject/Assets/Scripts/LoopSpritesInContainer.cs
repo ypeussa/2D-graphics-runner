ff683fff33eeeaf820dac4f4dfd48b06f50305fd
yrjo.peussa <peussa@iki.fi>
2017-03-29 17:56:40 +0300

    looping backgrounds

---
+++ unityproject/Assets/Scripts/LoopSpritesInContainer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class LoopSpritesInContainer : MonoBehaviour {
+
+    public List<Transform> sprites;
+    float spriteWidth;
+    public float warpThreshold = 1f;
+
+	void Start () {
+		foreach (var t in transform) {
+            sprites.Add((Transform)t);
+        }
+        sprites = sprites.OrderBy(x => x.position.x).ToList();
+        var sr = sprites[0].GetComponent<SpriteRenderer>();
+        spriteWidth = sr.bounds.size.x; // assume all are same size
+        //spriteWidth -= 0.1f;
+	}
+	
+	void Update () {
+        var cam = Camera.main;
+        var camP = cam.transform.position;
+        var halfCW = cam.orthographicSize * cam.aspect;
+        Debug.DrawLine(Vector3.up + camP, Vector3.up + camP + Vector3.right * halfCW);
+        // right case
+        // if camera's right edge close enough to rightmost sprite's right edge
+        if (sprites[sprites.Count - 1].position.x + spriteWidth * 0.5f - (camP.x + halfCW) < warpThreshold) {
+            sprites[0].position = sprites[sprites.Count-1].position + Vector3.right * spriteWidth;
+            var newRight = sprites[0];
+            sprites.RemoveAt(0);
+            sprites.Add(newRight);
+        }
+        // left case
+        // if camera's left edge close enough to leftmost sprite's left edge
+        if (camP.x - halfCW - (sprites[0].position.x - spriteWidth * 0.5f) < warpThreshold) {
+            sprites[sprites.Count-1].position -= Vector3.right * spriteWidth * sprites.Count;
+            var newLeft = sprites[sprites.Count-1];
+            sprites.RemoveAt(sprites.Count-1);
+            sprites.Insert(0, newLeft);
+        }
+    }
+}
